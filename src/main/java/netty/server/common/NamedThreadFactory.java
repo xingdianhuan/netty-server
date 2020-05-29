7327c66d3eaef481d74dea5c17d0ffc431ebf5a0
h_xing <h_xing@trip.com>
2020-05-29 18:28:38 +0800

    123

--- src/main/java/netty/server/common/NamedThreadFactory.java
+++ src/main/java/netty/server/common/NamedThreadFactory.java
@@ -8,6 +8,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class NamedThreadFactory implements ThreadFactory {
 
     private final static Map<String, AtomicInteger> PREFIX_COUNTER = new ConcurrentHashMap<>();
+    private final String prefix;
+    private final int totalSize;
+    private final Boolean makeDaemons;
+
+    public NamedThreadFactory(String prefix, int totalSize, Boolean makeDaemons) {
+        PREFIX_COUNTER.putIfAbsent(prefix,new AtomicInteger(0));
+        int prefixCounter = PREFIX_COUNTER.get(prefix).incrementAndGet();
+        this.prefix = prefix+"_"+prefixCounter;
+        this.totalSize = totalSize;
+        this.makeDaemons = makeDaemons;
+    }
 
     @Override
     public Thread newThread(Runnable r) {
